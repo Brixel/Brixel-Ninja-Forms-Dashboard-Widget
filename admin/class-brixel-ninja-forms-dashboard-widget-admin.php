5af09f544891e0f6e0b4ecb37762554f021dd0e9
Wouter Vandenneucker <wouter@vandenneucker.be>
2015-09-16 21:29:52 +0200

    changes

--- admin/class-brixel-ninja-forms-dashboard-widget-admin.php
+++ admin/class-brixel-ninja-forms-dashboard-widget-admin.php
@@ -136,7 +136,24 @@ class Brixel_Ninja_Forms_Dashboard_Widget_Admin {
 		echo '</table>';
 		*/
 
-		echo "bonkers";
+		echo "bonkers2";
+		$custom_post_type = 'nf_sub';
+		$args=array(
+		  'post_type' => $custom_post_type,
+		  'post_status' => 'publish',
+		  'posts_per_page' => -1,
+		  'caller_get_posts'=> 1
+		);
+		$my_query = null;
+		$my_query = new WP_Query($args);
+		if( $my_query->have_posts() ) {
+		  echo 'List of Submissions';
+		  while ($my_query->have_posts()) : $my_query->the_post(); ?>
+		    <p><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></p>
+		    <?php
+		  endwhile;
+		}
+		wp_reset_query();  // Restore global post data stomped by the_post().
 
 	}
 }
